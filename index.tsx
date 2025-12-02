08be47e7a3f3abcd6e5ab1e5e7e593258567dc6f
lockarddesignco <will@lockard.co>
2025-12-02 11:32:59 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// This line connects your Tailwind styles
+import './src/app/globals.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
