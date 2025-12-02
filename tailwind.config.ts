4086634608269832c249e41a568e7ebe0c90827e
lockarddesignco <will@lockard.co>
2025-12-02 11:45:23 -0500

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)', 'sans-serif'],
-        display: ['var(--font-oswald)', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+        display: ['Oswald', 'sans-serif'],
       },
       colors: {
         neon: {
